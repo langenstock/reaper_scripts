f633e5d1a0d4f528c2aeb1f529cc81fce7cc2d8d
David Lauritsen <david.lauritsen@bigant.com>
2024-04-03 13:35:57 +1100

    increment play speed up or down scripts

---
+++ play_speed_increment_down.lua
@@ -0,0 +1,37 @@
+-- Note that this script will turn off Loop Source (item properties) upon touching an item
+
+local incrementAmount = 0.1
+local incrementDirection = -1
+
+function main()
+    local activeProj = 0
+    
+    -- Count selected items
+    local numSelItems = reaper.CountSelectedMediaItems(activeProj)
+    
+    if numSelItems > 0 then
+        for i = 0, numSelItems - 1 do
+            -- Get reference to item
+            local item = reaper.GetSelectedMediaItem(activeProj, i)
+            
+            -- Get main take of item
+            local take = reaper.GetActiveTake(item)
+            
+            -- Turn off Loop Source
+            local falso = 0
+            reaper.SetMediaItemInfo_Value(item, "B_LOOPSRC", falso)            
+            
+            -- Get current playrate value of take
+            local currentPlayRate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
+            
+            local newvalue = currentPlayRate + (incrementAmount * incrementDirection)
+            reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", newvalue)
+        end
+    end
+    
+    -- Call UpdateTimeline to have the changes appear immediately on screen
+    reaper.UpdateTimeline()
+end
+
+
+main()
