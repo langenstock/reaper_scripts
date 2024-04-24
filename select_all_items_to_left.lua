db4c2436e8a4d1b9f3057a87b6f2e3e602f2c105
David Lauritsen <david.lauritsen@bigant.com>
2024-04-24 10:58:11 +1000

    scripts: sel all items to left or current selection, and likewise for right of sel

---
+++ select_all_items_to_left.lua
@@ -0,0 +1,31 @@
+function main()
+    local activeProj = 0
+    
+    local numOfSelItems = reaper.CountSelectedMediaItems(activeProj)
+    
+    if numOfSelItems > 0 then
+        -- Get first selected item
+        local firstItem = reaper.GetSelectedMediaItem(activeProj, 0)
+        
+        local firstItemPos = reaper.GetMediaItemInfo_Value(firstItem, "D_POSITION")
+    
+        local thisItemsTrack = reaper.GetMediaItemTrack(firstItem)
+    
+        local numOfItems = reaper.CountMediaItems(activeProj)
+        
+        for i = 0, numOfItems - 1 do
+            local item = reaper.GetMediaItem(activeProj, i)
+            
+            if reaper.GetMediaItemTrack(item) == thisItemsTrack then
+                if reaper.GetMediaItemInfo_Value(item, "D_POSITION") < firstItemPos then
+                    reaper.SetMediaItemSelected(item, true)
+                end
+            end
+        end
+        
+        reaper.UpdateTimeline()
+    end
+end
+
+
+main()
