e9d555077437c52706360d3f81ac0cc7caf1b748
David Lauritsen <david.lauritsen@bigant.com>
2024-04-26 13:56:51 +1000

    update to script for making regions from current item name

--- create_regions_named_filenames_from_selected_items.lua
+++ create_regions_named_filenames_from_selected_items.lua
@@ -6,64 +6,58 @@
 function main()
     local project = 0 -- 0 represents the active project, change if needed
 
-    -- Get the total number of selected items in the project
     local itemCount = reaper.CountSelectedMediaItems(project)
 
-    -- Check if there are any items in the project
-    if itemCount == 0 then
-        reaper.ShowMessageBox("No selected items found.", "Error", 0)
-        return
-    end
-    
-    -- Get front buffer time
-    _, userInputFrontBuffer = reaper.GetUserInputs('Time at front of region?', 1, '', '')
-    local frontEndBuffer = 0
-    if tonumber(userInputFrontBuffer) then
-        frontEndBuffer = tonumber(userInputFrontBuffer)
-    end
-    
-    -- Get back end buffer time
-    _, userInputBackEndBuffer = reaper.GetUserInputs('Time at end of region?', 1, '', '')
-    local backEndBuffer = 0
-    if tonumber(userInputBackEndBuffer) then
-        backEndBuffer = tonumber(userInputBackEndBuffer)
-    end
-
-    -- Iterate through each item and create a region
-    for i = 0, itemCount - 1 do
-        local item = reaper.GetSelectedMediaItem(project, i)
+    if itemCount > 0 then
+  
+        -- Get front buffer time
+        _, userInputFrontBuffer = reaper.GetUserInputs('Time at front of region?', 1, '', '')
+        local frontEndBuffer = 0
+        if tonumber(userInputFrontBuffer) then
+            frontEndBuffer = tonumber(userInputFrontBuffer)
+        end
         
-        local itemMuteState = reaper.GetMediaItemInfo_Value(item, "B_MUTE")
-        local muteStateValue = 1
-        
-        -- If the item is muted then skip this one
-        if itemMuteState ~= muteStateValue then
-            local active_take = reaper.GetActiveTake(item)
-            local _, take_name = reaper.GetSetMediaItemTakeInfo_String(active_take, "P_NAME", "", false)
+        -- Get back end buffer time - use for including reverb tails etc.
+        _, userInputBackEndBuffer = reaper.GetUserInputs('Time at end of region?', 1, '', '')
+        local backEndBuffer = 0
+        if tonumber(userInputBackEndBuffer) then
+            backEndBuffer = tonumber(userInputBackEndBuffer)
+        end
+    
+        -- Iterate through each item and create a region
+        for i = 0, itemCount - 1 do
+            local item = reaper.GetSelectedMediaItem(project, i)
             
-            local position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-            local length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+            local itemMuteState = reaper.GetMediaItemInfo_Value(item, "B_MUTE")
+            local muteStateValue = 1
             
-            local regionStart = position - frontEndBuffer
-            -- make sure we are not going left of 0
-            regionStart = math.max(0, regionStart)
-            
-            local regionEnd = position + length + backEndBuffer
-            
-            -- Create a region for each item
-            local isrgn = true
-            local regionIndex = reaper.AddProjectMarker2(project, isrgn, regionStart, regionEnd, take_name, -1, 0)
-            if regionIndex == -1 then
-              reaper.ShowMessageBox("Whoopsied.", "Error", 0)
+            -- If the item is muted then skip this one
+            if itemMuteState ~= muteStateValue then
+                local active_take = reaper.GetActiveTake(item)
+                local _, take_name = reaper.GetSetMediaItemTakeInfo_String(active_take, "P_NAME", "", false)
+                
+                local position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+                local length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+                
+                local regionStart = position - frontEndBuffer
+                -- make sure we are not going left of 0
+                regionStart = math.max(0, regionStart)
+                
+                local regionEnd = position + length + backEndBuffer
+                
+                -- Create a region for each item
+                local isrgn = true
+                local regionIndex = reaper.AddProjectMarker2(project, isrgn, regionStart, regionEnd, take_name, -1, 0)
+                if regionIndex == -1 then
+                  reaper.ShowMessageBox("Whoopsied.", "Error", 0)
+                end
             end
         end
+    
+        reaper.UpdateArrange()
     end
-
-    -- Update the arrange view to reflect the changes
-    reaper.UpdateArrange()
-
 end
 
--- Run the function
+
 main()
 
